9c5362aa63bc8e17d8569d0302879d42788c184e
Lucas Castro <lucas.sc98@hotmail.com>
2021-10-04 11:55:04 -0300

    Understanding how gatsby-link works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,6 +8,16 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <Link to="/" activeStyle={{ color: "red" }}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
