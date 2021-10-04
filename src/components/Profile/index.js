a9946975150fa3a0347d3cbf8c32d0dcad12f6a4
Lucas Castro <lucas.sc98@hotmail.com>
2021-10-04 12:11:06 -0300

    Creating a component and sharing between pages

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="profile-wrapper">
+    <h1>Lucas Castro</h1>
+    <h2>Desenvolvedor FrontEnd</h2>
+    <p>Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá </p>
+  </div>
+)
+
+export default Profile
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const AboutPage = () => (
-    <Layout>
-        <h1>About page</h1>
-    </Layout>
+  <Layout>
+    <SEO title="About" />
+    <h1>About page</h1>
+  </Layout>
 )
 
 export default AboutPage
