a2fb2c97f8007a0ab691bae3eddb314ebf78c22b
Lucas Castro <lucas.sc98@hotmail.com>
2021-10-19 16:38:54 -0300

    Insert MenuLink component on Sidebar

--- src/components/MenuLinks/index.js
+++ src/components/MenuLinks/index.js
@@ -7,13 +7,13 @@ import * as S from "./styled"
 const MenuLinks = () => (
   <S.MenuLinksWrapper>
     <S.MenuLinksList>
-      {links.map((link, i) => {
-        ;<S.MenuLinksItem key={i}>
+      {links.map((link, i) => (
+        <S.MenuLinksItem key={i}>
           <S.MenuLinksLink to={link.url} activeClassName="active">
             {link.label}
           </S.MenuLinksLink>
         </S.MenuLinksItem>
-      })}
+      ))}
     </S.MenuLinksList>
   </S.MenuLinksWrapper>
 )
