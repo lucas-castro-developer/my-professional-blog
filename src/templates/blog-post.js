4d1b1702732087539f90db906e753e337c5f11d1
Lucas Castro <lucas.sc98@hotmail.com>
2022-08-09 21:14:13 -0300

    Add base style to the post component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,16 +4,25 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import * as S from "../components/Post/styled"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
     <>
       <Layout>
-        <SEO title={post.frontmatter.title}>
-          <h1>{post.frontmatter.title}</h1>
+        <SEO title={post.frontmatter.title} />
+        <S.PostHeader>
+          <S.PostDate>
+            {post.frontmatter.date} • {post.timeToRead} min de leitura
+          </S.PostDate>
+          <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+          <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+        </S.PostHeader>
+        <S.MainContent>
           <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-        </SEO>
+        </S.MainContent>
       </Layout>
     </>
   )
@@ -24,8 +33,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
